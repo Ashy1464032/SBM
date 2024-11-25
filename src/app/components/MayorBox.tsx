9dda22e66c42c78853828518f9b710aa1335fd5f
Ashy1464032 <ashy1464032@gmail.com>
2024-11-25 18:14:17 +0000

    Basic Features and Minimum Viable Product

---
+++ src/app/components/MayorBox.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from "react";
+
+interface MayorProps {
+    name: string;
+    perks?: {
+        description: ReactNode;name: string
+}[];
+}
+
+export default function MayorBox({name, perks}: MayorProps) {
+    return (
+        <div className="mt-20 mx-auto border-4 border-stone-600 rounded-lg border-double shadow-2xl w-[50ch] justify-center">
+            <h2 className="text-3xl underline font-semibold">Mayor:</h2>
+            <h3 className="text-2xl">{name}</h3>
+            <h2 className="text-3xl underline font-semibold">Perks:</h2>
+            <ol>
+                {perks && perks.map((perk, index) => (
+                    <li key={index}>
+                        {index + 1}. {perk.name}
+                        <p className="max-w-[50ch] text-center" style={{textAlign: 'center'}}>{perk.description}</p>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    )
+}
