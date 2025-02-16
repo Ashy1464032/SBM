c89807566a34ad4f59e03d921998b4d50ec5fadf
Ashy1464032 <ashy1464032@gmail.com>
2025-02-16 20:26:44 +0000

    Changed scalling issues on mobile and removed the minecraft formatting codes from text

--- src/app/components/MayorBox.tsx
+++ src/app/components/MayorBox.tsx
@@ -3,13 +3,14 @@ import React, { ReactNode } from "react";
 interface MayorProps {
     name: string;
     perks?: {
-        description: ReactNode;name: string
+        description: ReactNode;
+        name: string
 }[];
 }
 
 export default function MayorBox({name, perks}: MayorProps) {
     return (
-        <div className="mt-20 mx-auto border-4 border-stone-600 rounded-lg border-double shadow-2xl w-[50ch] justify-center">
+        <div className="mt-20 mx-auto border-4 border-stone-600 rounded-lg border-double shadow-2xl w-full sm:w-[50ch] justify-center">
             <h2 className="text-3xl underline font-semibold">Mayor:</h2>
             <h3 className="text-2xl">{name}</h3>
             <h2 className="text-3xl underline font-semibold">Perks:</h2>
@@ -17,7 +18,10 @@ export default function MayorBox({name, perks}: MayorProps) {
                 {perks && perks.map((perk, index) => (
                     <li key={index}>
                         {index + 1}. {perk.name}
-                        <p className="max-w-[50ch] text-center" style={{textAlign: 'center'}}>{perk.description}</p>
+
+                        <p className="max-w-[50ch] text-center" style={{textAlign: 'center'}}>
+                            {String(perk.description).replace(/§(.)/g, "")}
+                        </p>
                     </li>
                 ))}
             </ol>
